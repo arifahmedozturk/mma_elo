12a9822ff7270a44500c54af0d0c6e28f91d0ad7
unknown <arif.ozturk.ichc@gmail.com>
2023-07-10 23:24:10 +0100

    added system argvs to scrape event

--- scrape_event.py
+++ scrape_event.py
@@ -1,10 +1,18 @@
+import sys
 from WikiScraper.WikiScraper import WikiScraper
 from DB import Fight
+
+if len(sys.argv) < 2:
+    raise Exception("Event URL not provided")
+url = sys.argv[1]
+
+if 'https://en.wikipedia.org/wiki/' not in url:
+    raise Exception("URL is not valid")
 
 FightDB = Fight.Fight()
 
 wiki_scraper = WikiScraper()
-matches = wiki_scraper.get_event_matches('https://en.wikipedia.org/wiki/UFC_290')
+matches = wiki_scraper.get_event_matches(url)
 for match in matches:
     FightDB.add_fight(match['fighter'], match['opponent'], match['result'], match['date'], match['method'], match['event'])
     print(f"Added fight: {match['fighter']} VS. {match['opponent']} - {match['method']}")
