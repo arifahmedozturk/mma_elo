feeee59e87cacbce42bd2937237585126c50323b
unknown <arif.ozturk.ichc@gmail.com>
2023-07-16 15:35:45 +0100

    bugfix

--- Config/Config.py
+++ Config/Config.py
@@ -2,7 +2,7 @@ class Config:
     def __init__(self):
         self.AGE_PENALTY = 50
         self.ELO_MULTIPLIER = 200
-        self.ELO_NORMALIZATION_FACTOR = 400
+        self.ELO_NORMALIZATION_FACTOR = 600
         self.KO_PENALTY = 25
         self.MIN_AGE_FOR_PENALTY = 38
         self.STARTING_ELO = 1200
